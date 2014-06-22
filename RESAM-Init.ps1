79d52e9f0ad66b0716a18867041f5134b0bec45e
lucdekens <dekens.luc@gmail.com>
2014-06-22 02:17:26 +0200

    Create RESAM-Init.ps1

---
+++ RESAM-Init.ps1
@@ -0,0 +1,73 @@
+# RESAM module - enumerations
+
+$jobStatus = @"
+using System;
+ 
+namespace RESAM
+{
+  [FlagsAttribute]
+  public enum JobStatus {
+    Unknown = -2,
+    Disabled = -1,
+    Scheduled = 0,
+    Active = 1,
+    Aborting = 2,
+    Aborted = 3,
+    Completed = 4,
+    Failed = 5,
+    FailedHalted = 6,
+    Canceled = 7,
+    CompletedWithErrors = 8,
+    Skipped = 9,
+    TimedOut = 11
+  }
+}
+"@
+Add-Type -TypeDefinition $jobStatus -Language CSharpVersion3
+
+$jobWhatType = @"
+using System;
+ 
+namespace RESAM
+{
+  [FlagsAttribute]
+  public enum JobWhatType {
+    Module = 0,
+    Project = 1,
+    Runbook = 2
+  }
+}
+"@
+Add-Type -TypeDefinition $jobWhatType -Language CSharpVersion3
+
+$jobWhoType = @"
+using System;
+ 
+namespace RESAM
+{
+  [FlagsAttribute]
+  public enum JobWhoType {
+    Agent = 0,
+    Team = 1
+  }
+}
+"@
+Add-Type -TypeDefinition $jobWhoType -Language CSharpVersion3
+
+$jobParameterType = @"
+using System;
+ 
+namespace RESAM
+{
+  [FlagsAttribute]
+  public enum JobParameterType {
+    Text = 0,
+    List = 1,
+    Credentials = 2,
+    MultiSelectList = 3,
+    MultiLineText = 4,
+    Password = 5
+  }
+}
+"@
+Add-Type -TypeDefinition $jobParameterType -Language CSharpVersion3
